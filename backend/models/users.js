ed3f51081306800e5f9447911dd8adcd138e2734
ayanbajo <bolatitoayanbajo@gmail.com>
2023-01-31 20:38:48 -0500

    working on deployment

--- backend/models/users.js
+++ backend/models/users.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose
 const userSchema = new Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
-    email: { type: String },
+    email: { type: String, required: true },
     password: { type: String }
 
 })
