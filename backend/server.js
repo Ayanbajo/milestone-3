f297093c1cc7f43aba7bef973bd376fbd3e75655
ayanbajo <bolatitoayanbajo@gmail.com>
2023-01-31 21:39:52 -0500

    working on deployment

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,6 @@ app.use(express.static(path.join(__dirname, "../frontend", 'build')));
 //   res.send("Hey there!!!");
 // });
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../frontend", 'build', 'index.html'));
-});
-
 mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
@@ -32,6 +28,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use("/article", postRoute)
 app.use("/users", userRoute)
 app.use("/auth", authRoute);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend", 'build', 'index.html'));
+});
 
 
 
